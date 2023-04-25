f935eca5632bccaa0c52bfd14a27317d98cbdb21
Sagar Kushwaha <128670927+Sagar-Kushwahaa@users.noreply.github.com>
2023-04-25 23:50:11 +0545

    Add files via upload

---
+++ first_day-python.py
@@ -0,0 +1,34 @@
+print("sagar")
+
+print("Hello World")
+
+print("Day 1-Python Print Function")
+print("The Function Is Decleared Like This")
+print("pritn('What to print')")
+
+print("Hello"+" "+"Sagar")
+
+print("Day 1-string manipulation")
+print('sting concatenation is done with the "+" sign.')
+print('e.g print ("Hello"+"World")')
+print("new lines can be create with a backslash and n.")
+
+#input()
+print("Hello"+" "+input("what is your name?")+"!")
+
+print( len(input ("What is your name? ") ) )
+
+name="sagar" +" "+"roshan"
+print(name)
+name= input ("What is your name? ")
+length=len(name)
+print(length)
+
+a=input("a:")
+b=input("b:")
+c=a
+a=b
+b=c
+print("a="+a)
+print("b="+b)
+
